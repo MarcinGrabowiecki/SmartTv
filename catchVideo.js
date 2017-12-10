0cf1aa3f1fe54bd8e2591b0c25723c26e92c5cd4
Marcin Grabowiecki <marcin.grabowiecki@cgm.com>
2017-12-10 11:13:36 +0100

    jqueryDetect2

--- catchVideo.js
+++ catchVideo.js
@@ -1,6 +1,7 @@
 //javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://rawgit.com/MarcinGrabowiecki/SmartTv/master/catchVideo.js';})();
 
-if($){}else{
+
+if($==undefined){
 	console.log(1111)
 
 
