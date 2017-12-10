9d62025f9a7f2cc2b54c83c1944bc9f379e3ba68
Marcin Grabowiecki <marcin.grabowiecki@cgm.com>
2017-12-10 10:46:13 +0100

    catcher_maybe_works

--- catchVideo.js
+++ catchVideo.js
@@ -6,3 +6,20 @@ console.log($('video')[0]);
 var src=$('video')[0].src;
 var title=document.title;
 
+//https://script.google.com/macros/s/AKfycbwMQEd6FI91OgLpKBzejeYkkFhSCarWrUh3giJJlxUE42VBr8mJ/exec?command=append&data=[1,2,3,"xxxxxxx"]
+var url="https://script.google.com/macros/s/AKfycbwMQEd6FI91OgLpKBzejeYkkFhSCarWrUh3giJJlxUE42VBr8mJ/exec"
+
+$.ajax({
+  url: url,
+  type: "get",
+  data: { 
+  	command: 'append', 
+  	data: JSON.stringify([title,src,'play'])
+  },
+  success: function(response) {
+    console.log(response);
+  },
+  error: function(xhr) {
+    console.log(xhr);
+  }
+});
