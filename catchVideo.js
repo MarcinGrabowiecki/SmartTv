94137418aee9d6c47694365cd303a817f874a501
Marcin Grabowiecki <marcin.grabowiecki@cgm.com>
2017-12-10 11:08:24 +0100

    jqueryDetect1

--- catchVideo.js
+++ catchVideo.js
@@ -1,11 +1,24 @@
 //javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://rawgit.com/MarcinGrabowiecki/SmartTv/master/catchVideo.js';})();
 
 if($){}else{
-	var fileref = document.createElement('script')
-	fileref.setAttribute("type", "text/javascript")
-	fileref.setAttribute("src", "https://code.jquery.com/jquery-3.2.1.min.js")
-	document.getElementsByTagName("head")[0].appendChild(fileref)
+	console.log(1111)
+
+
+(function(){
+  var newscript = document.createElement('script');
+     newscript.type = 'text/javascript';
+     newscript.async = true;
+     newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js';
+  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
+})();
+
+	// var fileref = document.createElement('script')
+	// fileref.setAttribute("type", "text/javascript")
+	// fileref.setAttribute("src", "https://code.jquery.com/jquery-3.2.1.min.js")
+	// document.getElementsByTagName("head")[0].appendChild(fileref)
 }
+
+
 console.log($('video')[0]);
 
 var src;
