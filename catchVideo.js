d36454302b50ad21c9a01167c0f07633cc8e03df
Marcin Grabowiecki <marcin.grabowiecki@cgm.com>
2017-12-10 10:58:26 +0100

    xhm_patch

--- catchVideo.js
+++ catchVideo.js
@@ -3,7 +3,10 @@
 console.log($);
 console.log($('video')[0]);
 
-var src=$('video')[0].src;
+var src;
+if($('video')[0]) src=$('video')[0].src;
+else if($('video')) src=$('video').src;
+
 var title=document.title;
 
 //https://script.google.com/macros/s/AKfycbwMQEd6FI91OgLpKBzejeYkkFhSCarWrUh3giJJlxUE42VBr8mJ/exec?command=append&data=[1,2,3,"xxxxxxx"]
